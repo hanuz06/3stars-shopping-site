67dc50eb648fcc4c3b6b215f6ffc34405d4bc37c
Andrey Li <lee1975uz@gmail.com>
2020-07-25 18:42:27 -0600

    add sticky header functionality

--- src/components/LowerNavbar/index.tsx
+++ src/components/LowerNavbar/index.tsx
@@ -1,66 +1,85 @@
 import React, { useState } from "react";
 import { useStyles } from "./style";
-import { Layout, Menu, Typography, Avatar } from "antd";
+import { Layout, Menu, Typography, Input } from "antd";
 import { NavLink } from "react-router-dom";
 import logo from "../../images/3stars.png";
-import { AppstoreOutlined, UserOutlined } from '@ant-design/icons';
+import { AppstoreOutlined, UserOutlined } from "@ant-design/icons";
 
 const { Header } = Layout;
 const { SubMenu } = Menu;
 const { Text } = Typography;
+const { Search } = Input;
 
 const LowerNavbar = () => {
   const [current, setCurrent] = useState("");
+  const [searchWord, setSearchWord] = useState("");
 
   const classes = useStyles();
 
   const handleClick = (e: any) => {
     setCurrent(e.key);
-  };  
+  };
+
+  const hangleSearch = (word: string) => {
+    setSearchWord(word);
+  };
 
   return (
-    <Header className={classes.upperContainer}>
-      <div className={classes.container}>
-        <NavLink exact to="/">
-          <img
-            src={logo}
-            alt="logo"
-            style={{ width: "60px", height: "auto" }}
+    <span className="sticky-inner">
+      <Header className={classes.upperContainer}>
+        <div className={classes.container}>
+          <NavLink exact to="/">
+            <img
+              src={logo}
+              alt="logo"
+              style={{ width: "60px", height: "auto" }}
+            />
+          </NavLink>
+          <Search
+            placeholder="input search text"
+            onSearch={(value: string) => console.log(value)}
+            enterButton
+            size="large"
+            allowClear
+            onChange={(e) => hangleSearch(e.target.value)}
+            value={searchWord}
+            maxLength={30}
+            className={classes.searchWindow}
           />
-        </NavLink>
 
-        <Menu
-          onClick={handleClick}
-          // selectedKeys={[current]}
-          defaultSelectedKeys={["1"]}
-          mode="horizontal"
-          triggerSubMenuAction="hover"
-          style={{ backgroundColor: "#fff", width:500, maxWidth:'100%'}}
-        >
-          <Menu.Item key="1">
-            <NavLink exact to="/">
-              <Text>HOME</Text>
-            </NavLink>
-          </Menu.Item>
+          <Menu
+            onClick={handleClick}
+            // selectedKeys={[current]}
+            defaultSelectedKeys={["1"]}
+            mode="horizontal"
+            triggerSubMenuAction="hover"
+            style={{ backgroundColor: "#fff", width: 500, maxWidth: "100%" }}
+          >
+            <Menu.Item key="1">
+              <NavLink exact to="/">
+                <Text>HOME</Text>
+              </NavLink>
+            </Menu.Item>
 
-          <SubMenu key="sub1" icon={<AppstoreOutlined />} title="SHOP">
-            <Menu.Item key="2">GADGETS</Menu.Item>
-            <Menu.Item key="3">ACCESSORIES</Menu.Item>
-          </SubMenu>
+            <SubMenu key="sub1" icon={<AppstoreOutlined />} title="SHOP">
+              <Menu.Item key="2">GADGETS</Menu.Item>
+              <Menu.Item key="3">ACCESSORIES</Menu.Item>
+            </SubMenu>
 
-          <SubMenu key="sub2" icon={<UserOutlined />} title="MY ACCOUNT">
-            <Menu.Item key="4">CART</Menu.Item>
-            <Menu.Item key="5">CHECKOUT</Menu.Item>
-          </SubMenu>
+            <SubMenu key="sub2" icon={<UserOutlined />} title="MY ACCOUNT">
+              <Menu.Item key="4">CART</Menu.Item>
+              <Menu.Item key="5">CHECKOUT</Menu.Item>
+            </SubMenu>
 
-          <Menu.Item key="6">
-            <NavLink to="#">
-              <Text>CONTACT US</Text>
-            </NavLink>
-          </Menu.Item>
-        </Menu>
-      </div>
-    </Header>
+            <Menu.Item key="6">
+              <NavLink to="#">
+                <Text>CONTACT US</Text>
+              </NavLink>
+            </Menu.Item>
+          </Menu>
+        </div>{" "}
+      </Header>
+    </span>
   );
 };
 
--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -11,30 +11,31 @@ import LandingPage from "./LandingPage";
 import LoginPage from "./LoginPage";
 import SignupPage from "./SignupPage";
 import CheckoutPage from "./CheckoutPage";
-import UpperNavbar from "../components/UpperNavbar";
 
 import { Layout } from "antd";
 import HeaderContainer from "./HeaderContainer";
 
 const { Header, Content, Footer } = Layout;
 
-const App: FC = () => (
-  <Router>
-    <div className="App">
-      <Layout>
-        <HeaderContainer />
-        <Layout>
-          <Switch>
-            <Route exact path="/" component={LandingPage} />
-            <Route path="/login" component={LoginPage} />
-            <Route path="/signup" component={SignupPage} />
-            <Route path="/checkout" component={CheckoutPage} />
-          </Switch>
-          <Footer style={{ textAlign: "center" }}>3stars Corp., 2020</Footer>
-        </Layout>
-      </Layout>
-    </div>
-  </Router>
-);
+const App: FC = () => {
 
+  return (
+    <Router>
+      <div className="App">
+        <Layout>
+          <HeaderContainer />
+          <Layout>
+            <Switch>
+              <Route exact path="/" component={LandingPage} />
+              <Route path="/login" component={LoginPage} />
+              <Route path="/signup" component={SignupPage} />
+              <Route path="/checkout" component={CheckoutPage} />
+            </Switch>
+            <Footer style={{ textAlign: "center" }}>3stars Corp., 2020</Footer>
+          </Layout>
+        </Layout>
+      </div>
+    </Router>
+  );
+};
 export default App;
